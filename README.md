1d411331e20dca4965136fe2c373bb012c9d01f3
akikrahman <akik_r@yahoo.co.uk>
2022-05-24 12:09:52 +0100

    updated for dev macbook

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # macbook-software
  
-### A bash script to install tools used by DTS Developers & A&P Data Engineers
+### A bash script to install tools used by Developers
  
 Click the Launchpad (Rocket icon) and search for Terminal. Open a terminal – The Mac terminal seems to be almost identical to a Linux Bash Shell. Like using WSL on Windows.
  
 Enter the following into the terminal:
  
-- Create folder called Projects % `mkdir Projects`
-- Navigate to the new folder % `cd Projects`
-- Install Git % `git init`
-- Clone the repo to download the files % `git clone https://github.com/hmcts/macbook-software`
+- Create folder called Projects % `mkdir macbook-software`
+- Navigate to the new folder % `copy the setup.sh script to the folder`
 - Navigate to the macbook-software folder % `cd macbook-software`
 - Make it executable % `chmod +x setup.sh`
 - Run the script % `./setup.sh`
@@ -17,41 +15,16 @@ Enter the following into the terminal:
 As the script runs it will prompt several times for admin password to proceed with an install. The admin password is the one you use to login to the Macbook, not your AppleId password.
 
 You can also use the simple following one-liner in the terminal to run the script without needing to download, set permissions etc.
-`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/hmcts/macbook-software/master/setup.sh)"`
+`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/...setup.sh)"`
  
 ### The software to be installed is:
  
-**For A&P users:**
- 
+**For Developers:**
+- brew
 - git
-- npm
-- postgresql
-- python3
-- azure-cli
+- maven
+- spring-boot
 - docker
-- pycharm
-- azure-data-studio
-- microsoft-azure-storage-explorer
-- R
-- rstudio
-- visual-studio
-- visual-studio-code
-- slack
+- microsoft-teams
 - google-chrome
-- pgadmin4
- 
-**For DTS Developers:**
- 
-- git
-- npm
-- postgresql
-- python3
-- azure-cli
-- yq
-- docker
-- adoptopenjdk
-- intellij-idea
-- pycharm
-- slack
-- google-chrome
-- pgadmin4
+
--- setup.sh
+++ setup.sh
@@ -1,53 +1,36 @@
 #!/bin/bash
 
-#software for DTS Developers
+#software for Developers
 developers(){
     # Collection of packages to install
     PACKAGES=(
         git
-        node@14
-        postgresql@12
-        libpq
-        python3
-        azure-cli
-        yq
+        maven
+        spring-boot
     )
     # Same packages, but a Seperate list for uninstall packages, as the order may need to be different
     PACKAGES_UNINSTALL=(
-        azure-cli
         git
-        node@14
-        postgresql@12
-        libpq
-        python3        
-        yq
+        maven
+        spring-boot
     )
     echo "$MODE-ing packages..."
     doBrewPackageInstallOrUninstall;
     echo "Cleaning up..."
     brew cleanup
 
-
     #now install casks
     CASKS=(
         docker
-        adoptopenjdk
-        intellij-idea
-        pycharm
-        slack
+        microsoft-teams
         google-chrome
-        pgadmin4
         parallels
     )
     # Same Casks, but a Seperate list for uninstall casks, as the order may need to be different
     CASKS_UNINSTALL=(
         docker
-        adoptopenjdk
-        intellij-idea
-        pycharm
-        slack
+        microsoft-teams
         google-chrome
-        pgadmin4
     )
     echo "$MODE-ing cask apps..."
     doBrewCaskInstallOrUninstall;
@@ -61,154 +44,6 @@ developers(){
     else
         echo "Finished the install"
     fi
-}
-
-#software for A&P Data Engineers
-dataengineers(){
-    # Collection of packages to install
-    PACKAGES=(
-        git
-        npm
-        postgresql
-        python3
-        azure-cli
-    )
-    # Same packages, but a Seperate list for uninstall packages, as the order may need to be different
-    PACKAGES_UNINSTALL=(
-        azure-cli
-        git
-        npm
-        postgresql
-        python3
-    )
-
-    echo "$MODE-ing packages..."
-    doBrewPackageInstallOrUninstall;
-    echo "Cleaning up..."
-    brew cleanup
-    
-    #now install casks
-    CASKS=(
-        docker
-        pycharm
-        azure-data-studio
-        microsoft-azure-storage-explorer
-        R
-        rstudio
-    #    visual-studio
-        visual-studio-code
-    #    virtualbox
-        slack
-        google-chrome
-        pgadmin4
-        parallels
-    )
-    # Same Casks, but a Seperate list for uninstall casks, as the order may need to be different
-    CASKS_UNINSTALL=(
-        docker
-        pycharm
-        azure-data-studio
-        microsoft-azure-storage-explorer
-        R
-        rstudio
-    #    visual-studio
-        visual-studio-code
-    #    virtualbox
-        slack
-        google-chrome
-        pgadmin4
-    )
-
-    echo "$MODE-ing cask apps..."
-    doBrewCaskInstallOrUninstall;
-    echo "Cleaning up..."
-    brew cleanup
-
-    #if the mode is to uninstall, ensure that the uninstall of parallels is done, as a special case.
-    if [ "$MODE" = "uninstall" ]
-    then
-        parallels_uninstall
-    else
-        echo "Finished the install"
-    fi
-}
-
-#all software
-allsoftware(){
-        # Collection of packages to install
-        PACKAGES=(
-            git
-            npm
-            postgresql
-            libpq
-            python3
-            azure-cli
-            yq
-        )
-        # Same packages, but a Seperate list for uninstall packages, as the order may need to be different
-        PACKAGES_UNINSTALL=(
-            azure-cli
-            git
-            npm
-            postgresql
-            libpq
-            python3
-            yq
-        )
-
-        echo "$MODE-ing packages..."
-        doBrewPackageInstallOrUninstall;
-        echo "Cleaning up..."
-        brew cleanup
-
-        #now install casks
-        CASKS=(
-            adoptopenjdk
-            intellij-idea
-            docker
-            pycharm
-            azure-data-studio
-            microsoft-azure-storage-explorer
-            R
-            rstudio
-            visual-studio
-            visual-studio-code
-        #    virtualbox
-            slack
-            google-chrome
-            pgadmin4
-            parallels
-        )
-        # Same Casks, but a Seperate list for uninstall casks, as the order may need to be different
-        CASKS_UNINSTALL=(
-            adoptopenjdk #password may be necessary
-            intellij-idea
-            docker #password alwyas seems to be required
-            pycharm
-            azure-data-studio
-            microsoft-azure-storage-explorer
-            R #password may be necessary
-            rstudio
-            visual-studio
-            visual-studio-code
-        #    virtualbox
-            slack
-            google-chrome
-            pgadmin4
-        )
-
-        echo "$MODE-ing cask apps..."
-        doBrewCaskInstallOrUninstall;
-        echo "Cleaning up..."
-        brew cleanup
-
-        #if the mode is to uninstall, ensure that the uninstall of parallels is done, as a special case.
-        if [ "$MODE" = "uninstall" ]
-        then
-            parallels_uninstall
-        else
-            echo "Finished the install"
-        fi
 }
 
 #parallels uninstall special case
@@ -222,17 +57,14 @@ parallels_uninstall(){
 
 cat << "EOF2"
 The uninstall has now finished.....
-
 **** Steps to complete Post uninstall of parallels ****
 Please read the below...
 The uninstall of parallels should have completed. However, due to an anomoly with the way that parallels installs
 , the uninstall may or may not have reported an error. Please follow the instructions below carefully..
-
 you may have seen something like this at the end of the parallels uninstall...
 --------------------------------------------------------------------------
 Cleaning up...
 Error: Permission denied @ apply2files - /usr/local/share/man/man8/prl_convert.8
-
 Steps to carry out to resolve any errors...
 ------------------------------------------
 1. Try and check the health of brew. Try to install something that you havent already got, and see if brew complains. eg. try and install jq.
@@ -299,60 +131,24 @@ install_homebrew(){
 
 clear
 # Bootstrap script for setting up a new OSX machine
-# for Digital Delivery devs and QAs.
 
-# Version 0.1 - Thomas Geraghty
-# Version 0.2 - Thomas Geraghty
-# Version 0.3 - Sandy Gudgeon
-# Version 0.4 - Thomas Geraghty
-# Version 0.5 - Rajiv Kapoor (added uninstall functionality)
-
-cat << "EOF"
- _   _ ___  ________ _____ _____   _____________
-| | | ||  \/  /  __ \_   _/  ___|  |   _.+._   |
-| |_| || .  . | /  \/ | | \ `--.   | (^\/^\/^) |
-|  _  || |\/| | |     | |  `--. \  |  \@*@*@/  |
-| | | || |  | | \__/\ | | /\__/ /  |  {_____}  |
-\_| |_/\_|  |_/\____/ \_/ \____/   |___________|
- ____  _____ _____ 
-|    \|_   _|   __|     Welcome to Digital Delivery         
-|  |  | | | |__   |     Inspiring change and lookin' cool   
-|____/  |_| |_____|     whilst we do it!                    
-EOF
 echo
-echo "DTS Macbook Development Tools Installer V0.3"
+echo "Macbook Development Tools Installer"
 echo ""
-echo "Any problems email :"
-echo "Thomas Geraghty - thomas.geraghty@justice.gov.uk for DTS Developer/QA queries"
-echo "Alexander Gudgeon - alexander.gudgeon@justice.gov.uk for A&P Data Engineering queries"
 echo ""
 ECHO "Pease enter your choice: "
-options=("Install DTS Developer Tools" "Install A&P Data Engineer Tools" "Install All Tools" "*UNINSTALL DTS Developer Tools" "*UNINSTALL A&P Data Engineer Tools" "*UNINSTALL All Tools" "Quit")
+options=("Install Developer Tools"  "*UNINSTALL Developer Tools" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "Install DTS Developer Tools")
+        "Install Developer Tools")
             echo "you chose to install tools for $opt - $REPLY"
             MODE=install
             install_homebrew
             developers
             break
             ;;
-        "Install A&P Data Engineer Tools")
-            echo "you chose to install tools for $opt - $REPLY"
-            MODE=install
-            install_homebrew
-            dataengineers
-            break
-            ;;
-        "Install All Tools")
-            echo "you chose choice to install $opt - $REPLY"
-            MODE=install
-            install_homebrew
-            allsoftware
-            break
-            ;;
-        "*UNINSTALL DTS Developer Tools")
+        "*UNINSTALL Developer Tools")
             echo "you chose to Uninstall tools for $opt - $REPLY"
             MODE=uninstall
             #check that folks are sure whether they wish to uninstall....
@@ -368,42 +164,9 @@ do
             fi
             break
             ;;
-        "*UNINSTALL A&P Data Engineer Tools")
-            echo "you chose to Uninstall tools for $opt - $REPLY"
-            MODE=uninstall
-            #check that folks are sure whether they wish to uninstall....
-            echo -n "Answer \"yes\" if you are sure you wish to uninstall..."
-            read ANSWER
-            if [ "$ANSWER" = "yes" ]
-            then
-                #even though we are uninstalling we still need to install brew
-                install_homebrew
-                dataengineers
-            else
-                echo "Bailing out..."
-            fi
-            break
-            ;;
-        "*UNINSTALL All Tools")
-            echo "you chose choice to Uninstall $opt - $REPLY"
-            MODE=uninstall
-            #check that folks are sure whether they wish to uninstall....
-            echo -n "Answer \"yes\" if you are sure you wish to uninstall..."
-            read ANSWER
-            if [ "$ANSWER" = "yes" ]
-            then
-                #even though we are uninstalling we still need to install brew
-                install_homebrew
-                allsoftware
-            else
-                echo "Bailing out..."
-            fi
-            break
-            ;;
         "Quit")
             break
             ;;
         *) echo "invalid option $REPLY";;
     esac
 done
-
